cce54f9ce42611f5d5a587cbc315c4aeb8a7f3b4
Alex Şuhan <asuhan@google.com>
2019-05-30 17:07:53 -0700

    More ROI align memory usage improvements

--- tests/test_roi_align.py
+++ tests/test_roi_align.py
@@ -2,15 +2,18 @@ from maskrcnn_benchmark.layers import ROIAlign, tensor_roi_align
 import random
 import torch
 import torch_xla
+import torch_xla_py.xla_model as xm
 import unittest
 
+
+NUM_ROIS = 512
 
 class TestROIAlign(unittest.TestCase):
 
   def _gen_rois(self, batch_size, width, height, rois_count):
     rois = []
     for i in range(0, rois_count):
-      batch_id = float(int(random.random() * batch_size))
+      batch_id = float(int(i / NUM_ROIS))
       start_w = 0.5 * random.random() * width
       start_h = 0.5 * random.random() * height
       end_w = (0.5 + 0.5 * random.random()) * width
@@ -22,8 +25,8 @@ class TestROIAlign(unittest.TestCase):
     return torch.rand([batch_size, channels, width, height])
 
   def test_roi_align(self):
-    rois_count = 512
-    batch_size = 8
+    batch_size = 1
+    rois_count = NUM_ROIS * batch_size
     width = 64
     height = 64
     channels = 1024
@@ -31,13 +34,15 @@ class TestROIAlign(unittest.TestCase):
     spatial_scale = .5
     sampling_ratio = 2
     rois = self._gen_rois(batch_size, width, height, rois_count)
+    rois_3d = rois[:, 1:].reshape(batch_size, NUM_ROIS, -1)
     bottom_image_data = self._gen_data(batch_size, channels, width, height)
     roi_align = ROIAlign(
         output_size, spatial_scale=spatial_scale, sampling_ratio=sampling_ratio)
     aligned_ref = roi_align(bottom_image_data, rois)
-    rois = rois.to(device='xla')
-    bottom_image_data = bottom_image_data.to(device='xla')
-    aligned = tensor_roi_align(bottom_image_data, rois, output_size,
+    xla_device = xm.xla_device()
+    rois_3d = rois_3d.to(device=xla_device)
+    bottom_image_data = bottom_image_data.to(device=xla_device)
+    aligned = tensor_roi_align(bottom_image_data, rois_3d, output_size,
                                spatial_scale, sampling_ratio)
     self.assertAlmostEqual(
         (aligned - aligned_ref).abs().max().item(), 0, places=5)
